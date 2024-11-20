192259d09147926081dd2cc31b0e3fbfcdca42fb
Ali Yasser <aly.yaser@acmesaico.net>
2024-11-20 20:14:45 +0200

    Enhance code quality

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
+"""
+This module contains a function to print the current application version
+and exit the program.
+"""
+import sys
+VERSION="v0.0.1"
 def current_app_ver(version):
+    '''
+    Function print system Version then Exit
+    '''
     print(version)
-    exit()
-version="v0.0.1"
-current_app_ver(version)
+    sys.exit()
+current_app_ver(VERSION)
